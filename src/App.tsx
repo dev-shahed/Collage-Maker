5c66ba1e4c911c3c02e4fdf324fc31779939ff19
dev-shahed <shahedthedev@gmail.com>
2024-12-30 17:50:50 +0600

    fix responsive issue

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,18 @@ import './styles/collage.css';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Toolbar />
-      <div className="flex">
-        <div className="w-64 border-r min-h-screen p-4 space-y-6">
-          <StyleSelector />
-          <AIEffects />
-        </div>
-        <div className="flex-1">
-          <CollageEditor />
-        </div>
+    <Toolbar />
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-60 border-b md:border-r p-4 space-y-6 md:min-h-screen">
+        <StyleSelector />
+        <AIEffects />
       </div>
-      <ExportOptions />
+      <div className="flex-1">
+        <CollageEditor />
+      </div>
     </div>
+    <ExportOptions />
+  </div>
   );
 }
 
