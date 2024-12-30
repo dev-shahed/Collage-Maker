3b5b52378385c9517829aeb207dd7c7fbbbfc180
dev-shahed <shahedthedev@gmail.com>
2024-12-30 17:56:32 +0600

    responsive issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
     <div className="min-h-screen bg-gray-50">
     <Toolbar />
     <div className="flex flex-col md:flex-row">
-      <div className="w-full md:w-60 border-b md:border-r p-4 space-y-6 md:min-h-screen">
+      <div className="w-full md:w-64 border-b md:border-r p-4 space-y-6 md:min-h-screen">
         <StyleSelector />
         <AIEffects />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 sm-m-4">
         <CollageEditor />
       </div>
     </div>
